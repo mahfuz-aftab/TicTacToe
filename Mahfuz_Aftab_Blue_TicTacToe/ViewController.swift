7600587230bbd300b1d75849a1adf65fd7c48dec
mahfuz-aftab <mahfuz.cracker@gmail.com>
2022-09-27 23:44:43 -0400

    FEAT:: W4P1:: Embed different view using Navigation Controller and used Table View Controller

---
+++ Mahfuz_Aftab_Blue_TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mahfuz_Aftab_Blue_TicTacToe
+//
+//  Created by MAHFUZ AFTAB on 2022-09-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
